e46bcf38a1e7d39b3f75689f379980f5b82f0c52
MarkoPetr <tvojemail@domena.com>
2025-12-12 00:49:20 +0100

    Fix Tab1Page imports for Appflow build

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,18 +1,18 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { Tab1Page } from './tab1.page';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    IonicModule,
+    Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
