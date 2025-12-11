c264d3d89703c30cb522b25d7b6713100b22dfb5
MarkoPetr <tvojemail@domena.com>
2025-12-11 23:15:04 +0100

    Dodaj CUSTOM_ELEMENTS_SCHEMA da build prepozna Ionic komponente

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -15,5 +16,6 @@ import { Tab1Page } from './tab1.page';
     Tab1PageRoutingModule
   ],
   declarations: [Tab1Page]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1PageModule {}
