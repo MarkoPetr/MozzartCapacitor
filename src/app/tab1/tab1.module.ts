57c2512d11c5d94ee808ce7824dd53410f16df5c
MarkoPetr <tvojemail@domena.com>
2025-12-11 23:44:07 +0100

    Fix Ionic imports + CUSTOM_ELEMENTS_SCHEMA for AppFlow build

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,21 +1,19 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { Tab1Page } from './tab1.page';
+import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
-    HttpClientModule,
     Tab1PageRoutingModule
   ],
-  declarations: [Tab1Page]
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1PageModule {}
