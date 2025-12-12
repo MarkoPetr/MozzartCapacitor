aa87da28e793ec2c026d728fe653a5d8d20cd1ae
MarkoPetr <tvojemail@domena.com>
2025-12-12 07:14:09 +0100

    Final fix for Ionic build (Tab1PageModule)

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,7 +1,8 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule, NgIf, NgForOf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { Tab1Page } from './tab1.page';
 
@@ -10,9 +11,7 @@ import { Tab1Page } from './tab1.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab1PageRoutingModule,
-    NgIf,
-    NgForOf
+    Tab1PageRoutingModule
   ],
   declarations: [Tab1Page],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
