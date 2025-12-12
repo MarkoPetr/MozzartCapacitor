ca0f06ca3a12e4e0bc65e6fb02a26676ac18e4d9
MarkoPetr <tvojemail@domena.com>
2025-12-12 06:40:54 +0100

    Fix Ionic components with CUSTOM_ELEMENTS_SCHEMA

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule, NgIf, NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -14,6 +14,7 @@ import { Tab1Page } from './tab1.page';
     NgIf,
     NgForOf
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1PageModule {}
